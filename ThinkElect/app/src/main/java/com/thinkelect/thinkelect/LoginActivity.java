cb98d41fbab9f91af9c62dde53b70130aef4d329
Micah Herrera <micah@micahherrera.com>
2016-08-10 10:59:48 -0500

    wednesday start

--- ThinkElect/app/src/main/java/com/thinkelect/thinkelect/LoginActivity.java
+++ ThinkElect/app/src/main/java/com/thinkelect/thinkelect/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.thinkelect.thinkelect;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.candidate_item_card);
+
+        CircleDisplay cd = (CircleDisplay) findViewById(R.id.circleDisplay);
+        cd.setAnimDuration(3000);
+        cd.setValueWidthPercent(30f);
+        cd.setTextSize(16f);
+        cd.setColor(Color.YELLOW);
+        cd.setDrawText(true);
+        cd.setDrawInnerCircle(true);
+        cd.setFormatDigits(0);
+        cd.setTouchEnabled(false);
+        cd.setUnit("%");
+        cd.setStepSize(0.5f);
+        // cd.setCustomText(...); // sets a custom array of text
+        cd.showValue(25f, 100f, true);
+
+
+
     }
 }
